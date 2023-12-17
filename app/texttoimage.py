53efc088cd75232520e30a1323edd6fcc20e2042
Pete <github@kthxbye.us>
2023-12-17 21:36:09 +0100

    CHG: model to realistic vision

--- app/texttoimage.py
+++ app/texttoimage.py
@@ -323,12 +323,13 @@ class DiffusersTextToImage(ImagePromptInterface):
  
 
     def process(self, prompt):
-        model_id = "runwayml/stable-diffusion-v1-5"
+        #model_id = "runwayml/stable-diffusion-v1-5"
+        model_id = "SG161222/Realistic_Vision_V6.0_B1_noVAE"
         pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16, safety_checker=None, use_safetensors=True)
 
         img_num = 1
         img_list = []
-        for image in pipe(prompt).images:
+        for image in pipe(f"{prompt} ### {self._negativePrompt}").images:
             # Create a BytesIO object to store the PNG data
             png_bytes_io = io.BytesIO()
 
