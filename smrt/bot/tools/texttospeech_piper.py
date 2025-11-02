822e0a262a34aa5ca187886aad9c3526824f6212
Pete <github@kthxbye.us>
2025-11-02 01:01:56 +0100

    FIX: compatibilty with path and wave.open

--- smrt/bot/tools/texttospeech_piper.py
+++ smrt/bot/tools/texttospeech_piper.py
@@ -1,6 +1,7 @@
 import logging
 from pathlib import Path
 import wave
+import os
 from piper import PiperVoice
 
 from .texttospeech import TextToSpeechInterface
@@ -12,7 +13,8 @@ class PiperTTSModel(TextToSpeechInterface):
         logging.debug(f"Creating xtts model from {self._onnx_path}")
     
     def tts(self, text: str, output_wav_file : Path|str, language : str = None) -> bool:
+        output_wav_file = Path(output_wav_file)
         voice = PiperVoice.load(self._onnx_path)
-        with wave.open(output_wav_file, "wb") as wav_file:
+        with wave.open(os.fspath(output_wav_file), "wb") as wav_file:
             voice.synthesize_wav(text, wav_file)
         return True
