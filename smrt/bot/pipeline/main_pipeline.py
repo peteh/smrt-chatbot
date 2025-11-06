ff5f2fb19454855eedade065c126346793c37b87
Pete <github@kthxbye.us>
2025-11-06 18:44:55 +0100

    CHG: cleanup pipe

--- smrt/bot/pipeline/main_pipeline.py
+++ smrt/bot/pipeline/main_pipeline.py
@@ -6,16 +6,13 @@ from .pipeline import PipelineInterface, HelpPipeline
 class MainPipeline():
     def __init__(self):
         self._help_pipeline = HelpPipeline()
-        #self._talk_pipeline = pipeline.TalkPipeline(questionbot_mistral_nemo)
-        self._self_pipelines = []
+        self._self_pipelines: list[PipelineInterface] = []
         self._pipelines = [#self._talk_pipeline,
                     self._help_pipeline]
-        #self._talk_pipeline.set_pipelines(self._pipelines)
         self._help_pipeline.set_pipelines(self._pipelines)
 
     def add_pipeline(self, pipe: PipelineInterface):
         self._pipelines.append(pipe)
-        #self._talk_pipeline.set_pipelines(self._pipelines)
         self._help_pipeline.set_pipelines(self._pipelines)
 
     def add_self_pipeline(self, pipe: PipelineInterface):
@@ -32,7 +29,7 @@ class MainPipeline():
                     thread = threading.Thread(target=self.process_pipe, args=(pipe, messenger_instance, message))
                     thread.start()
             return
-        
+
         for pipe in self._pipelines:
             if pipe.allowed_in_chat_id(messenger_instance, message) and pipe.matches(messenger_instance, message):
                 logging.debug(f"Pipe {type(pipe).__name__} matches, processing")
@@ -40,4 +37,3 @@ class MainPipeline():
                 thread.start()
             # delete message from phone after processing
             #whatsapp.deleteMessage(message)
-
