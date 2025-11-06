ebb2860be636e2dc6e573b16cdba4f2e67cc612c
Pete <github@kthxbye.us>
2025-11-06 17:17:37 +0100

    FIX: log message for piper

--- smrt/bot/tools/texttospeech_piper.py
+++ smrt/bot/tools/texttospeech_piper.py
@@ -10,7 +10,7 @@ from .texttospeech import TextToSpeechInterface
 class PiperTTSModel(TextToSpeechInterface):
     def __init__(self, onnx_path: Path|str) -> None:
         self._onnx_path = Path(onnx_path)
-        logging.debug(f"Creating xtts model from {self._onnx_path}")
+        logging.debug(f"Creating piper model from {self._onnx_path}")
     
     def tts(self, text: str, output_wav_file : Path|str, language : str = None) -> bool:
         output_wav_file = Path(output_wav_file)
