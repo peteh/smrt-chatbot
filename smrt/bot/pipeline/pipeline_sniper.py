926ce156c70e2368c9f7491102a5f42243b65cde
Pete <github@kthxbye.us>
2025-11-25 22:29:10 +0100

    FIX: ignore new stuff at restart

--- smrt/bot/pipeline/pipeline_sniper.py
+++ smrt/bot/pipeline/pipeline_sniper.py
@@ -32,40 +32,47 @@ class NetcupScheduledTask(AbstractSniperTask):
     def __init__(self, messenger_manager, chat_ids):
         super().__init__(messenger_manager, chat_ids, "de-DE")
         logging.info("Initialized Netcup Scheduled Task")
-        self._old_products = {}
 
     def new_session(self):
         session = requests.Session()
         session.headers.update(self._headers)
         return session
+    
+    def get_products(self, session: requests.Session):
+        r = session.get("https://www.netcup.com/de/deals/black-friday")
+        if "unschlagbare Rabatte. Sei dabei und sichere dir dein Schnäppchen!" not in r.text:
+            logging.debug("Netcup Black Friday page not ready, waiting...")
+            raise Exception("Netcup Black Friday page not ready")
+        new_products = {}
+        soup = BeautifulSoup(r.text, "html.parser")
+        # Select all elements with the class "deal-card-container"
+        cards = soup.select(".deal-card-container")
+        for card in cards:
+            #print(card)           # the full element
+            h3 = card.find("h3")
+            product = h3.get_text(strip=True)
+            a = card.find("a", href=True)
+            product_link = f"https://www.netcup.com/{a['href']}"
+            new_products[product] = product_link
+        return new_products
 
     def run(self):
         counter = 0
         session = self.new_session()
+        # initialize old products with current state
+        old_products = self.get_products(session)
+
         while True:
             try:
                 counter += 1
                 if counter % 40 == 0:
                     session = self.new_session()
 
-                r = session.get("https://www.netcup.com/de/deals/black-friday")
-                if "unschlagbare Rabatte. Sei dabei und sichere dir dein Schnäppchen!" not in r.text:
-                    logging.debug("Netcup Black Friday page not ready, waiting...")
-                    raise Exception("Netcup Black Friday page not ready")
-                new_products = {}
-                soup = BeautifulSoup(r.text, "html.parser")
-                # Select all elements with the class "deal-card-container"
-                cards = soup.select(".deal-card-container")
-                for card in cards:
-                    #print(card)           # the full element
-                    h3 = card.find("h3")
-                    product = h3.get_text(strip=True)
-                    a = card.find("a", href=True)
-                    product_link = f"https://www.netcup.com/{a['href']}"
-                    new_products[product] = product_link
+                # grab current products
+                new_products = self.get_products(session)
 
                 # Figure out what's removed
-                for product, link in self._old_products.items():
+                for product, link in old_products.items():
                     if product not in new_products:
                         logging.debug(f"Netcup Product removed: {product} - {link}")
                         for chat_id in self.get_chat_ids():
@@ -74,12 +81,12 @@ class NetcupScheduledTask(AbstractSniperTask):
 
                 # Figure out what's new
                 for product, link in new_products.items():
-                    if product not in self._old_products:
+                    if product not in old_products:
                         logging.debug(f"Netcup New product found: {product} - {link}")
                         for chat_id in self.get_chat_ids():
                             messenger = self.get_messenger_manager().get_messenger_by_chatid(chat_id)
                             messenger.send_message(chat_id, f"Netcup Black Friday new product: {product} - {link}")
-                self._old_products = new_products.copy()
+                old_products = new_products.copy()
                 # wait for a bit before checking again
                 time.sleep(30)
             except Exception as e:
