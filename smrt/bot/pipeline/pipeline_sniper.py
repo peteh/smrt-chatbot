b1e4ffb3b830e1f1031b24734ff214ca07efbc7b
Pete <github@kthxbye.us>
2025-11-25 21:05:47 +0100

    FIX: use current user agend

--- smrt/bot/pipeline/pipeline_sniper.py
+++ smrt/bot/pipeline/pipeline_sniper.py
@@ -11,7 +11,7 @@ class AbstractSniperTask(AbstractScheduledTask):
 
         self._headers = {
             "User-Agent": (
-                "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:136.0) Gecko/20100101 Firefox/136.0"
+                "Mozilla/5.0 (X11; Linux x86_64; rv:144.0) Gecko/20100101 Firefox/144.0"
             ),
             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
             "Accept-Language": "de-DE,en;q=0.9",
